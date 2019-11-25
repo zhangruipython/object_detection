fbb8470f31aa0a8363df87e122c0df785051ff22
zhangruipython <3279164326@qq.com>
2019-11-25 10:20:52 +0800

    数据集划分为测试集和训练集

---
+++ cpu_version/base_apply/detect_video_cpu.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019-11-22 16:45
+@Author  : zhangrui
+@FileName: detect_video_cpu.py
+@Software: PyCharm
+解析视频，识别视频中每一帧
+"""
+from cpu_version.base_apply import detect_picture_cpu
+import cv2
+
+frame_write_path = "C:/rongze/picture/car/tyre/"  # 视频帧写入文件
+video_path = "C:/rongze/picture/car/big_car03.mp4"  # 视频地址
+frame_index = 0  # 视频帧文件名称
+
+names_path = "../data/coco.names"
+model_path = "../backup/yolov3.weights"
+cfg_path = "../cfg/yolov3.cfg"
+model_param = detect_picture_cpu.load_model(classes_file=names_path, model_configuration=cfg_path,
+                                            model_weights=model_path)
+
+capture = cv2.VideoCapture(video_path)
+while capture.isOpened():
+    ret, frame = capture.read()
+    if frame is None:
+        break
+    else:
+        frame_write_name = frame_write_path + str(frame_index) + ".jpg"
+        cv2.imwrite(frame_write_name, frame)
+        frame_index += 1
+        param = detect_picture_cpu.make_detect(model_param[0], model_param[1], frame_write_name)
+        detect_picture_cpu.draw_detection(param)
+capture.release()
--- download_data/splitTrainAndTest.py
+++ download_data/splitTrainAndTest.py
@@ -4,10 +4,10 @@
 @Author  : zhangrui
 @FileName: splitTrainAndTest.py
 @Software: PyCharm
+区分训练集和测试集
 """
 import random
 import os
-import sys
 
 
 def split_data_set(image_dir):
@@ -18,17 +18,17 @@ def split_data_set(image_dir):
     data_size = len(files)
 
     ind = 0
+    # 1/10的数据集作为测试集
     data_test_size = int(0.1 * data_size)
     test_array = random.sample(range(data_size), k=data_test_size)
-
     for f in os.listdir(image_dir):
         if f.split(".")[1] == "jpg":
             ind += 1
-
             if ind in test_array:
                 f_val.write(image_dir + '/' + f + '\n')
             else:
                 f_train.write(image_dir + '/' + f + '\n')
 
 
-split_data_set(sys.argv[1])
+data_path = "C:\\Users\\张睿\\Desktop\\ImgBiao"
+split_data_set(data_path)
